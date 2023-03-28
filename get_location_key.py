d7de006e0c12a03497e41226200955633974ffd8
Mikail Thoriq <mikail.thoriq@gmail.com>
2023-03-28 22:31:06 +0700

    better key fetching with included python script

--- get_location_key.py
+++ get_location_key.py
@@ -1,15 +1,23 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
-
 api_key = os.environ.get("api_key")
 location = os.environ.get("location")
 
-url = f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={location}"
+# get location key
+url = f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={location}&details=false"
 response = requests.get(url)
-data = response.json()
-simpendata = open("location.txt", "w")
-simpendata.write(str(data))
-simpendata.close()
+data = json.loads(response.text)
+
+# sanitize response
+sanitized_city = data[0]["LocalizedName"]
+location_key = data[0]["Key"]
+
+# output sanitized response
+print(f"your location key: {location_key}, and your city according to response is {sanitized_city}")
+
+
+
